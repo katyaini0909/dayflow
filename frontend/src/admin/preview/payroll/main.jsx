cdbd052acf087604a50a2115addaf090947117f8
yash26x <99918743+yash26x@users.noreply.github.com>
2026-08-22 13:12:39 +0530

    Build admin payroll page

---
+++ frontend/src/admin/preview/payroll/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Payroll from "../../pages/Payroll";
+import "../../AdminDashboard.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Payroll />
+);
