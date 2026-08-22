a054f8c5289933c16c57abc888315a99d0a77d8a
Katyaini <katyainisharma0909@gmail.com>
2026-08-22 15:51:02 +0530

    Add protected admin employee API

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
-
+const userRoutes = require("./routes/userRoutes");
+const adminRoutes = require("./routes/adminRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -13,7 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
 app.get("/", (req, res) => {
   res.json({ message: "Dayflow backend is running!" });
 });
